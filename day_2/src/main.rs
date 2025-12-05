c13cbfd48bf80986aecd832efbc1376cc3a35216
Malaka <ithayg@gmail.com>
2025-12-05 10:26:15 +0200

    day 2 part 2

--- day_2/src/main.rs
+++ day_2/src/main.rs
@@ -6,17 +6,21 @@ fn main() {
 
     if args.len() > 1 {
         let file_path = &args[1];
-        let sum = sum_invalid_ids(file_path);
+        let sum = sum_invalid_ids(file_path, false);
 
         println!("total sum: {sum}");
+
+        let new_sum = sum_invalid_ids(file_path, true);
+
+        println!("new rules total sum: {new_sum}");
     } else {
         println!("Error: no input file was provided");
     }
 }
 
-fn sum_invalid_ids(file_path: &String) -> u64 {
+fn sum_invalid_ids(file_path: &String, new_rules: bool) -> u64 {
     match fs::read_to_string(file_path) {
-        Ok(content) => sum_from_content(content),
+        Ok(content) => sum_from_content(content, new_rules),
         Err(err) => {
             println!("Error while reading file: {err}");
             0
@@ -24,7 +28,7 @@ fn sum_invalid_ids(file_path: &String) -> u64 {
     }
 }
 
-fn sum_from_content(mut content: String) -> u64 {
+fn sum_from_content(mut content: String, new_rules: bool) -> u64 {
     let mut ids_sum = 0;
     content.pop(); // '\n' at the end of the file
 
@@ -32,7 +36,7 @@ fn sum_from_content(mut content: String) -> u64 {
         if let Some((from, to)) = range.split_once('-') {
             if let Ok(from_num) = from.parse::<u64>() {
                 if let Ok(to_num) = to.parse::<u64>() {
-                    ids_sum += sum_id_nums(from_num, to_num);
+                    ids_sum += sum_id_nums(from_num, to_num, new_rules);
                 } else {
                     println!("error parsing number: {to}")
                 }
@@ -47,12 +51,18 @@ fn sum_from_content(mut content: String) -> u64 {
     ids_sum
 }
 
-fn sum_id_nums(from: u64, to: u64) -> u64 {
+fn sum_id_nums(from: u64, to: u64, new_rules: bool) -> u64 {
     let mut sum = 0;
+    let rules = if new_rules {
+        is_repeated_sequence
+    } else {
+        is_mirror
+    };
 
     for id in from..=to {
-        if is_mirror(id) {
+        if rules(id) {
             sum += id;
+            // println!("invalid id: {id}");
         }
     }
 
@@ -72,4 +82,21 @@ fn is_mirror(num: u64) -> bool {
     }
 
     res
+}
+
+fn is_repeated_sequence(num: u64) -> bool {
+    let str_num = num.to_string();
+    let mut double_str_num = str_num.clone();
+    double_str_num.push_str(&str_num);
+    let second_digit = &double_str_num[1..];
+
+    let match_index = second_digit.find(&str_num);
+
+    match match_index {
+        Some(index) => {
+            // println!("match index: {index}");
+            index < (str_num.len() - 1)
+        }
+        None => false,
+    }
 }
